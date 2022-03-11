7f1f88fb1b658afa1c9d963a2cce3f7972d90054
alx-pre_course <jospincarpusjospin@gmail.com>
2022-03-10 19:05:59 -0800

    My commit messsage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry point 
+*main - entry point
 *
 *Return: 0 after printing the function
 */
+int main(void)
 {
 puts("\"Programming is like bullding a multilingual puzzle");
 return (0);
