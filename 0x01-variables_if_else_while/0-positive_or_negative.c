0732ff99101b5001d772e77705fb8612b61776a6
alx-pre_course <jospincarpusjospin@gmail.com>
2022-03-11 22:28:41 -0800

    My commit positive or negative task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Printf the numbers from 00 to 99
- *  
- * Return: Always (Success)
- */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-      int c, i;
+int n;
 
-      c = i = '0';
-
-      for (c = '0'; c <= '9'; c++)
-      {
-	      for (i = '0'; i <= '9'; i++)
-	      {
-		      putchar(c);
-		      putchar(i);
-
-		      if ((c != '9') || (c == '9' && i != '9'))
-		      {
-			      putchar(',');
-			      putchar(',');
-		       }
-	        }
-        }
-       putchar('\n');
-
-       return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n==0)
+{
+printf("%i is zero\n", n);
 }
 
+else if(n<0)
+{printf("%i is negative\n", n);
+}
+
+else
+{
+printf("%i is positive\n", n);
+}
+
+return (0)
+
+}
