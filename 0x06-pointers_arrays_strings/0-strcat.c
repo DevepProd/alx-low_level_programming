7beb6ceed03249f6b0bb04efc35b9fb7d3d07ec4
alx-pre_course <jospincarpusjospin@gmail.com>
2022-03-23 16:24:28 -0700

    my first commit to task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+
+ ÂÂ*_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character tha will also be changed
+ *Return; dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = Ã 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
